9b874fc28173f1a85fdfceffd2f841f3ff473549
Guido Asbun <guido@asbun.io>
2021-09-07 21:45:55 -0700

    setup

---
+++ AnyList.h
@@ -0,0 +1,46 @@
+//
+// Created by Guido Asbun on 9/7/21.
+//
+
+#ifndef ANYLIST_H
+#define ANYLIST_H
+
+#include <string> // Need to include for nullptr.
+
+class Node
+{
+public:
+    Node() : data(0), ptrToNext(nullptr) {}
+    Node(int theData, Node *newPtrToNext)
+            : data(theData), ptrToNext(newPtrToNext){}
+    Node* getPtrToNext() const { return ptrToNext; }
+    int getData( ) const { return data; }
+    void setData(int theData) { data = theData; }
+    void setPtrToNext(Node *newPtrToNext)
+    { ptrToNext = newPtrToNext; }
+    ~Node(){}
+private:
+    int data;
+    Node *ptrToNext; // Pointer that points to next node.
+};
+
+
+class AnyList
+{
+public:
+    AnyList() : ptrToFirst(nullptr), count(0) {}
+
+    void insertFront(int);
+
+    void print() const;
+
+    void clearList();
+    ~AnyList();
+
+private:
+    Node *ptrToFirst;
+    // Pointer to point to the first node in the list.
+    int count;
+    // Variable to keep track of number of nodes in the list.
+};
+#endif //WEEK2LESSON1_ANYLIST_H
---
+++ cmake-build-debug/CMakeFiles/week2Lesson1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/week2Lesson1.dir/AnyList.cpp.o"
+  "CMakeFiles/week2Lesson1.dir/main.cpp.o"
+  "week2Lesson1"
+  "week2Lesson1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/week2Lesson1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
